a6aacb38755cc1365978eeb04d12a8469e058a72
maasencioh <maasencioh@gmail.com>
2017-03-11 11:53:18 -0500

    test(filter): inplace changes affected the next test

--- test/util/filter.js
+++ test/util/filter.js
@@ -1,8 +1,15 @@
 import test from 'ava';
-import {simple4 as chromatogram} from '../data/_examples.js';
-
+import {Chromatogram} from '../..';
 
 test('Filter: keep the odd', t => {
+    let chromatogram = new Chromatogram([1, 2, 3, 4], {
+        ms: [
+            [[101, 201, 301], [11, 21, 31]],
+            [[102, 202, 302], [12, 22, 32]],
+            [[103, 203, 303], [13, 23, 33]],
+            [[104, 204, 304], [14, 24, 34]],
+        ]
+    });
     chromatogram.filter((index) => index % 2);
     t.is(chromatogram.getTimes().length, 2);
     t.is(chromatogram.getSerie('ms').data.length, 2);
@@ -14,6 +21,14 @@ test('Filter: keep the odd', t => {
 });
 
 test('Filter: keep time under a value', t => {
+    let chromatogram = new Chromatogram([1, 2, 3, 4], {
+        ms: [
+            [[101, 201, 301], [11, 21, 31]],
+            [[102, 202, 302], [12, 22, 32]],
+            [[103, 203, 303], [13, 23, 33]],
+            [[104, 204, 304], [14, 24, 34]],
+        ]
+    });
     chromatogram.filter((index, time) => time < 3);
     t.is(chromatogram.getTimes().length, 2);
     t.is(chromatogram.getSerie('ms').data.length, 2);
