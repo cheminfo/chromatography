6c5286683c8960203927963da09fc210cf3c58f4
Luc Patiny <luc@patiny.com>
2017-03-16 15:56:07 +0100

    Add the documentation of 'integrate'

--- src/ms/integrate.js
+++ src/ms/integrate.js
@@ -2,47 +2,35 @@
 
 
 /**
- * Returns the closest mass spectrum to a specific retention time
- * @param {number} rt - Retention time
+ * Returns a mass spectrum that is the integration of all the spectra in a specific range of time
+ * When we combine 2 mass spectra we need to deal with the x axis (containing m/z)
+ * If the mass is closest than 'slot' to an existing mass in the new integrated spectrum
+ * then the 2 intensity are summed and the mass is proportionally averaged.
+ * We should take care that the resulting mass could theoretically be still closest to another peak
+ * and we could have to repeat this averaging (but this can only happen once)
+ * @param {number} from - From retention time
+ * @param {number} to - To retention time
  * @param {object} [options={}] - Options object
+ * @param {number} [options.slot] - Define when 2 peaks will be combined
  * @return {{rt: <number>, ms: <Array<Array<number>>>}}
  */
-function applyLockMass(mf, options) {
+
+
+// TODO this method should use 'getClosestTime'
+
+function integrate(from, to, options) {
     options = Object.assign({}, defaultOptions, options);
 
+    var fromIndex = this.getClosestTime(from).safeIndexBefore;
+    var toIndex = this.getClosestTime(to).safeIndexAfter;
+    var ms=[];
 
 
-
-    // check where is the reference values
-    let referenceIndexShift = Number(options.oddReference);
-    let msIndexShift = Number(!options.oddReference);
-
-    if (ms.length % 2 !== 0) {
-        throw new Error('The series must have an even size');
-    }
-
-    // applying the changes for all the spectra
-    for (let i = 0; i < ms.length >> 1; i++) {
-        let massIndex = 2 * i + msIndexShift;
-        let referenceIndex = 2 * i + referenceIndexShift;
-
-        // calculate the difference between theory and experimental (the smallest)
-        let difference = Number.MAX_VALUE;
-        for (let j = 0; j < referenceMass.length; j++) {
-            if (Math.abs(difference) > Math.abs(referenceMass[j] - ms[referenceIndex][0])) {
-                difference = referenceMass[j] - ms[referenceIndex][0];
-            }
-        }
-
-        for (let m = 0; m < ms[massIndex][0].length; m++) {
-            ms[massIndex][0][m] += difference;
-        }
-    }
-
-    // we remove the time and the mass spectra that containss the reference
-    this.filter((index) => index % 2 !== referenceIndexShift);
-
-    return this;
+    return {
+        fromIndex,
+        toIndex,
+        ms
+    };
 }
 
-module.exports = applyLockMass;
+module.exports = integrate;
