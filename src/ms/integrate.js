ea794242d4bca90185493fa7b96baf719b3553bd
maasencioh <maasencioh@gmail.com>
2017-03-24 17:59:24 -0500

    chore(integrate): work in progress

--- src/ms/integrate.js
+++ src/ms/integrate.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const getClosestTime = require('../util/getClosestTime');
+
+const defaultOptions = {
+    slot: 1
+};
+
 /**
  * Returns a mass spectrum that is the integration of all the spectra in a specific range of time
  * When we combine 2 mass spectra we need to deal with the x axis (containing m/z)
@@ -10,17 +16,42 @@
  * @param {Chromatogram} chrom
  * @param {number} from - From retention time
  * @param {number} to - To retention time
+ * @param {object} [options = {}] - Options object
+ * @param {number} [options.slot = 2] - Define when 2 peaks will be combined
  * @return {{fromIndex: number, toIndex: number, ms: Array}}
  */
-function integrate(chrom, from, to) {
-    var fromIndex = chrom.getClosestTime(from).safeIndexBefore;
-    var toIndex = chrom.getClosestTime(to).safeIndexAfter;
-    var ms = [];
+function integrate(chrom, from, to, options) {
+    options = Object.assign({}, defaultOptions, options);
+
+    const time = chrom.getTimes();
+    const ms = chrom.getSerie('ms');
+    var fromIndex = getClosestTime(from, time).safeIndexBefore;
+    var toIndex = getClosestTime(to, time).safeIndexAfter;
+
+    var masses = [];
+    var intensities = [];
+
+    // iterates over all masses
+    for (var i = fromIndex; i <= toIndex; i++) {
+        // iterates over mass-intensity
+        for (var j = 0; j < ms[i][0].length; j++) {
+            // search possible position
+            var position = {
+                index: -1,
+                distance: Number.MAX_VALUE
+            };
+            for (var k = 0; k < masses.length; k++) {
+                // TODO work in progress
+            }
+
+            // check if merge needed
+        }
+    }
 
     return {
         fromIndex,
         toIndex,
-        ms
+        ms: [masses, intensities]
     };
 }
 
