894b7133f404a930dbdd1f6895cbd5c880016f49
maasencioh <maasencioh@gmail.com>
2017-03-29 23:41:33 -0500

    feat(fromJSON): read from jcamp reader

--- src/from/json.js
+++ src/from/json.js
@@ -11,10 +11,17 @@ function fromJSON(json) {
     let series = json.series;
     let times = json.times;
     let chromatogram = new Chromatogram(times);
-    for (let key of Object.keys(series)) {
-        chromatogram.addSerie(key, series[key].data, {
-            meta: series[key].meta
-        });
+
+    if (Array.isArray(series)) {
+        for (let i = 0; i < series.length; i++) {
+            chromatogram.addSerie(series[i].name, series[i].data);
+        }
+    } else {
+        for (let key of Object.keys(series)) {
+            chromatogram.addSerie(key, series[key].data, {
+                meta: series[key].meta
+            });
+        }
     }
     return chromatogram;
 }
