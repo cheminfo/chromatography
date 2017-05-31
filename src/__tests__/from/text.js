fe4bd146f414f987553111eef99d7cd40a505560
Luc Patiny <luc@patiny.com>
2017-05-31 15:17:01 +0200

    Add test case

--- src/__tests__/from/text.js
+++ src/__tests__/from/text.js
@@ -14,3 +14,19 @@ test('Parse a text', () => {
     expect(newChromatogram.getSerieNames()).toEqual(['intensity']);
 });
 
+test('Parse a text with options', () => {
+    const text = `
+        1,2,3
+        2,3,4
+        3,4,5
+    `;
+
+    let newChromatogram = fromText(text, {
+        xColumn: 1,
+        yColumn: 2
+    });
+    expect(newChromatogram.getSerie('intensity').data.length).toEqual(3);
+    expect(newChromatogram.times.length).toEqual(3);
+    expect(newChromatogram.times).toEqual([2,3,4]);
+    expect(newChromatogram.getSerieNames()).toEqual(['intensity']);
+});
