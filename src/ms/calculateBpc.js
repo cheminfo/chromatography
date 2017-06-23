cb7db89024acc76ab6b4d6a4669a0ae8b8f399e1
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2017-06-23 15:35:33 +0200

    feat(calculateBpc): calculate base peak chromatogram

---
+++ src/ms/calculateBpc.js
@@ -0,0 +1,18 @@
+/**
+ * Calculate bpc
+ * @param {Chromatogram} chrom - GC/MS chromatogram where make the peak picking
+ * @return {Array} - Calculated bpc
+ */
+export function calculateBpc(chrom) {
+    let ms = chrom.getSerie('ms');
+    if (!ms) {
+        throw new Error('The mass serie must be defined');
+    }
+    var massSpectra = ms.data;
+    var bpc = [];
+    for (var massSpectrum of massSpectra) {
+        bpc.push(Math.max(...massSpectrum[1]));
+    }
+
+    return bpc;
+}
