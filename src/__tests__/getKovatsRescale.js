782c3923bb4d7d2e630e6d0134fe5c6b3832f0f4
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2017-08-09 17:04:48 +0200

    test: increase coverage

--- src/__tests__/getKovatsRescale.js
+++ src/__tests__/getKovatsRescale.js
@@ -15,3 +15,18 @@ test('triplet', () => {
     expect(kovatsRescale.kovatsIndexes).toEqual([{time: 15, value: 0}]);
     expect(kovatsRescale.peaks.length).toEqual(1);
 });
+
+test('inplace', () => {
+    let chrom = getSimulatedSpectrum();
+
+    const options = {
+        heightFilter: 2,
+        thresholdFactor: 0,
+        maxNumberPeaks: 1000,
+        groupWidth: 0
+    };
+
+    let kovatsRescale = chrom.getKovatsRescale(options);
+    expect(kovatsRescale.kovatsIndexes).toEqual([{time: 15, value: 0}]);
+    expect(kovatsRescale.peaks.length).toEqual(1);
+});
