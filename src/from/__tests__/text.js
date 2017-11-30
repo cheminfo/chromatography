70eea11686fa3ce80ddf44091fa14e2c268f599f
Luc Patiny <luc@patiny.com>
2017-11-30 16:18:55 +0100

    finishing moving tests

---
+++ src/from/__tests__/text.js
@@ -0,0 +1,32 @@
+const {fromText} = require('../..');
+
+test('Parse a text', () => {
+    const text = `
+        1,2
+        2,3
+        3,4
+    `;
+
+    let newChromatogram = fromText(text);
+    expect(newChromatogram.getSerie('intensity').data.length).toEqual(3);
+    expect(newChromatogram.times.length).toBe(3);
+    expect(newChromatogram.times).toEqual([1, 2, 3]);
+    expect(newChromatogram.getSerieNames()).toEqual(['intensity']);
+});
+
+test('Parse a text with options', () => {
+    const text = `
+        1,2,3
+        2,3,4
+        3,4,5
+    `;
+
+    let newChromatogram = fromText(text, {
+        xColumn: 1,
+        yColumn: 2
+    });
+    expect(newChromatogram.getSerie('intensity').data.length).toEqual(3);
+    expect(newChromatogram.times.length).toBe(3);
+    expect(newChromatogram.times).toEqual([2, 3, 4]);
+    expect(newChromatogram.getSerieNames()).toEqual(['intensity']);
+});
