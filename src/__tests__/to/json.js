1ab7ede36021e981cf4d0d5b6c11a7b1f88a25be
Luc Patiny <luc@patiny.com>
2017-11-30 16:17:21 +0100

    move to tests

--- src/__tests__/to/json.js
+++
@@ -1,8 +0,0 @@
-import {simple} from '../data/examples.js';
-
-test('Create a JSON', () => {
-    let json = JSON.stringify(simple);
-    expect(json).toContain('series');
-    expect(json).toContain('times":[1,2]');
-    expect(json).toContain('meta');
-});
