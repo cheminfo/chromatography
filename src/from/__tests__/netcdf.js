d2cb03498e63dfe6c773d91e311821cd9596040f
Luc Patiny <luc@patiny.com>
2018-11-05 13:18:20 +0100

    rename cdf test cases

--- src/from/__tests__/netcdf.js
+++ src/from/__tests__/netcdf.js
@@ -4,8 +4,17 @@ import { join } from 'path';
 import { fromNetCDF } from '../..';
 
 test('load NetCDF', () => {
-  const path = join(__dirname, '../../../testFiles/netcdf/test.cdf');
+  const path = join(__dirname, '../../../testFiles/netcdf/bruker-gcms.cdf');
   const netcdf = readFileSync(path);
   const chrom = fromNetCDF(netcdf);
+  expect(chrom.getSerieNames()).toEqual(['tic', 'ms']);
   expect(chrom).toHaveLength(4513);
+});
+
+test('load agilent HPLC cdf', () => {
+  const path = join(__dirname, '../../../testFiles/netcdf/agilent-hplc.cdf');
+  const netcdf = readFileSync(path);
+  const chrom = fromNetCDF(netcdf);
+  expect(chrom.getSerieNames()).toEqual(['uv254']);
+  expect(chrom).toHaveLength(4651);
 });
