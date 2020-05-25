3b2a84011fc52f45235f645c25bfcb7b3e206e45
Luc Patiny <luc@patiny.com>
2020-05-25 10:11:17 +0200

    docs: thining about new format

---
+++ examples/format.js
@@ -0,0 +1,36 @@
+/*
+2 possibilities for data:
+[1,2,3,4] or
+[{x:[], y:[]}]
+*/
+
+export const chromatogram = {
+  times: [], // renamed to data ???
+  meta: {},
+  series: [
+    {
+      data: [], // problem of 'sparse' series
+      meta: {},
+      name: 'tic',
+      kind: 'values',
+    },
+    {
+      data: [{ x: [], y: [] }], // one element per time = the higher level data
+      parent: [], //???
+      meta: {},
+      name: 'ms',
+      kind: 'ms',
+      parentIon: 123,
+      series: [
+        {
+          data: [{ x: [], y: [], time: Number }], // one element per previous data.x BUT only 14 peaks ...
+          kind: 'ms',
+          kind: 'ir',
+          parentIon: [],
+          meta: {},
+          name: 'ms',
+        },
+      ],
+    },
+  ],
+};j
