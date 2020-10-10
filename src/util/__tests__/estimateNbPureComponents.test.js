24c21f9bc36017fcd1bcb66e8f53676d3608a74f
jobo322 <jose.bolanos@correounivalle.edu.co>
2020-10-10 03:00:09 -0500

    fix: eslint

--- src/util/__tests__/estimateNbPureComponents.test.js
+++ src/util/__tests__/estimateNbPureComponents.test.js
@@ -8,6 +8,8 @@ test('Estimate number of pure components', () => {
   const path = join(__dirname, '../../../testFiles/jcamp/signalOverlap.jdx');
   const jcamp = readFileSync(path, 'utf8');
   const chromatogram = fromJcamp(jcamp);
-  let result = estimateNbPureComponents(chromatogram, { range: { from: 8.82, to: 9.2 } });
+  let result = estimateNbPureComponents(chromatogram, {
+    range: { from: 8.82, to: 9.2 },
+  });
   expect(result).toBe(2);
 });
