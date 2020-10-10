d002722120d458f7bd350a56f27cd50d3b4103eb
jobo322 <jose.bolanos@correounivalle.edu.co>
2020-10-10 00:44:29 -0500

    fix: estimateNbPureComponents function

--- src/util/__tests__/estimateNbPureComponents.test.js
+++ src/util/__tests__/estimateNbPureComponents.test.js
@@ -2,11 +2,12 @@ import { readFileSync } from 'fs';
 import { join } from 'path';
 
 import { fromJcamp } from '../..';
+import { estimateNbPureComponents } from '../estimateNbPureComponents';
 
 test('Estimate number of pure components', () => {
   const path = join(__dirname, '../../../testFiles/jcamp/signalOverlap.jdx');
   const jcamp = readFileSync(path, 'utf8');
   const chromatogram = fromJcamp(jcamp);
-  let result = chromatogram.estimateNbPureComponents({ from: 8.82, to: 9.2 });
+  let result = estimateNbPureComponents(chromatogram, { range: { from: 8.82, to: 9.2 } });
   expect(result).toBe(2);
 });
