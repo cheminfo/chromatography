c295c4b1a3b908b2ef1c1f42333b9948c867dd65
Michaël Zasso <targos@protonmail.com>
2022-03-01 13:55:31 +0100

    adapt to some ml-gsd changes

--- src/peaks/getPeaks.js
+++ src/peaks/getPeaks.js
@@ -10,21 +10,19 @@ export function getPeaks(chromatogram, options = {}) {
 
   const series = chromatogram.getSeries(seriesName).data;
   const times = chromatogram.getTimes();
+  const dataXY = { x: times, y: series };
   // first peak selection
-  let peakList = gsd(
-    { x: times, y: series },
-    {
-      noiseLevel: 0,
-      realTopDetection: false,
-      smoothY: true,
-      sgOptions: { windowSize: 5, polynomial: 2 },
-      heightFactor: 2,
-    },
-  );
+  let peakList = gsd(dataXY, {
+    noiseLevel: 0,
+    realTopDetection: false,
+    smoothY: true,
+    sgOptions: { windowSize: 5, polynomial: 2 },
+  });
+
   // filter height by factor
   let medianHeight = median(series);
 
-  peakList = peakList.filter((val) => val.height > medianHeight * heightFilter);
+  peakList = peakList.filter((val) => val.y > medianHeight * heightFilter);
 
   peakList.sort((a, b) => a.x - b.x);
 
@@ -33,12 +31,9 @@ export function getPeaks(chromatogram, options = {}) {
   }
 
   return peakList.map((peak) => ({
-    from: peak.from,
-    to: peak.to,
-    inflectionPoints: {
-      from: Math.min(peak.left.x, peak.right.x),
-      to: Math.max(peak.left.x, peak.right.x),
-    },
+    from: peak.x - peak.width / 2,
+    to: peak.x + peak.width / 2,
+    inflectionPoints: peak.inflectionPoints,
     retentionTime: peak.x,
     intensity: peak.y,
   }));
