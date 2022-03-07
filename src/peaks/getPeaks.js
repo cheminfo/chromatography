29aa55247c625c5d6d0f813fa54ddb5e9e968a5b
Luc Patiny <luc@patiny.com>
2022-03-07 18:25:46 +0100

    feat: getPeaks will make a baseline correction before calculate the noise

--- src/peaks/getPeaks.js
+++ src/peaks/getPeaks.js
@@ -1,9 +1,28 @@
-import median from 'ml-array-median';
+import airpls from 'ml-airpls';
 import { gsd, broadenPeaks } from 'ml-gsd';
+import { xMedianAbsoluteDeviation } from 'ml-spectra-processing';
+
+/**
+ * Returns the result of a peak picking in the chromatogram
+ *
+ * Make a peak picking on a chromatogram is not obvious because the baseline is often not close to 0
+ * and it is therefore difficult to filter by height.
+ * We will therefore consider as height of a peak 2 times the height between the top and the middle of the inflection points.
+ *
+ * We therefore calculate 2 properties:
+ * - the median that is expected ot be the
+ *
+ * @param {*} chromatogram
+ * @param {object} [options={}]
+ * @param {number} [options.heightFilter=2] Peak height should be this factor times the noise (Median Absolute Deviation)
+ * @param {string} [options.seriesName='tic']
+ * @param {object} [options.broadenPeaksOptions='tic']
+ * @returns
+ */
 
 export function getPeaks(chromatogram, options = {}) {
   const {
-    heightFilter = 2,
+    heightFilter = 5,
     seriesName = 'tic',
     broadenPeaksOptions = { factor: 1, overlap: false },
   } = options;
@@ -19,21 +38,30 @@ export function getPeaks(chromatogram, options = {}) {
     sgOptions: { windowSize: 5, polynomial: 2 },
   });
 
-  // filter height by factor
-  let medianHeight = median(series);
+  const noiseHeight =
+    xMedianAbsoluteDeviation(airpls(dataXY.x, dataXY.y).corrected).mad *
+    heightFilter;
 
-  peakList = peakList.filter((val) => val.y > medianHeight * heightFilter);
+
+
+  peakList = peakList.filter(
+    (peak) =>
+      (peak.y -
+        (dataXY.y[peak.inflectionPoints.from.index] +
+          dataXY.y[peak.inflectionPoints.to.index]) /
+          2) *
+        2 >
+      noiseHeight,
+  );
 
   peakList.sort((a, b) => a.x - b.x);
 
-  if (broadenPeaksOptions) {
-    peakList = broadenPeaks(peakList, broadenPeaksOptions);
-  }
+  const broadenPeaksList = broadenPeaks(peakList, broadenPeaksOptions);
 
-  return peakList.map((peak) => ({
-    from: peak.x - peak.width / 2,
-    to: peak.x + peak.width / 2,
-    inflectionPoints: peak.inflectionPoints,
+  return broadenPeaksList.map((peak) => ({
+    from: peak.from.x,
+    to: peak.to.x,
+    width: peak.width,
     retentionTime: peak.x,
     intensity: peak.y,
   }));
