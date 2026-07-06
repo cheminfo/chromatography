da4d6513387286f0b8fab9824b0c0516c7f3dfcc
Michaël Zasso <targos@protonmail.com>
2026-07-06 10:30:58 +0200

    chore: update linted folders

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,9 @@
-import { defineConfig , globalIgnores} from 'eslint/config'
+import { defineConfig, globalIgnores } from 'eslint/config';
 import cheminfo from 'eslint-config-cheminfo/base';
 
-export default defineConfig(
-  // globalIgnores([]),
-  cheminfo,
-);
+export default defineConfig(globalIgnores(['dist', 'lib']), cheminfo, {
+  files: ['examples/*.js'],
+  rules: {
+    'no-console': 'off',
+  },
+});
